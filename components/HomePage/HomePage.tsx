289220b9d6a697bd5011c2d7f9059d1a65d66cae
bluesnooker123 <dev333perfect@gmail.com>
2022-08-06 09:24:05 +0900

    refactor code

--- components/HomePage/HomePage.tsx
+++ components/HomePage/HomePage.tsx
@@ -6,7 +6,7 @@ const HomePage: React.FC = () => {
     <div className="container flex flex-col justify-center">
       <div className="flex flex-col max-w-lg h-96 p-8 md:w-full w-11/12 bg-gray-100 mx-auto justify-evenly rounded-3xl text-center items-center">
         <h2 className="text-5xl font-bold">Hello There!</h2>
-        <span className="text-base">Let's buy some insurance. It is going to take only a few step</span>
+        <span className="text-base">Let&apos;s buy some insurance. It is going to take only a few step</span>
         <Link href='/tellus'>
           <a>
             <button className="my-btn">Start</button>
